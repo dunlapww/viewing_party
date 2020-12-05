657a4d404aefefe7046af1b6db2f00a5f152ac48
Will Dunlap <dunlapww@gmail.com>
2020-12-05 12:12:53 -0700

    Add beginning of spec for new viewing party
    Co-authored-by: George Soderholm <Georgesoderholm@gmail.com>

--- spec/viewing_parties/new_spec.rb
+++ spec/viewing_parties/new_spec.rb
@@ -1,2 +1,19 @@
 require 'rails_helper'
 
+describe 'from the viewing party page' do
+  describe 'as a registered user' do
+    before :each do
+      @user = User.create(
+        email: 'testing@example.com',
+        password: '1234**USAusa',
+        password_confirmation: '1234**USAusa'
+      )
+      visit "/login"
+      fill_in 'email', with: 'testing@example.com'
+      fill_in 'password', with: '1234**USAusa'
+      click_on "Login"
+      
+    end
+  end
+  
+end
