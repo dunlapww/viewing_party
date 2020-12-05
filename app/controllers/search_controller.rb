864d6fc076b0bbf48c5d1cb0edda2599d407f410
Will Dunlap <dunlapww@gmail.com>
2020-12-05 16:56:22 -0700

    Add search controller and search results show view
    Co-authored-by: Kate Tester <Katemorris@gmail.com>
    Co-authored-by: George Soderholm <Georgesoderholm@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
-  def new; end
-
-  def show; end
+  def new
+  end
+  
+  def show
+    if params[:search]
+      @movies = MovieSearch.new(params[:search])
+    else
+      @movies = TopMovieSet.new
+    end
+  end
 end
