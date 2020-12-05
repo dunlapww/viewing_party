40de4688efba403295d13e12742591f318959934
Kate Tester <katemorris@gmail.com>
2020-12-04 21:14:23 -0700

    Adds friendship table to DB
    Co-authored-by: George Soderholm <georgesoderholm@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_011644) do
+ActiveRecord::Schema.define(version: 2020_12_05_040507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_12_02_011644) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "friendships", "users"
 end
