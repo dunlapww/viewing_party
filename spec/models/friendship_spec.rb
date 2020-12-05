412c4c3dd3c629e247b36eb698e94ee90b14a30e
Kate Tester <katemorris@gmail.com>
2020-12-04 21:15:22 -0700

    Adds relationships and tests for friendships
    Co-authored-by: George Soderholm <georgesoderholm@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should belong_to :friend }
+  end
+
+
+end
