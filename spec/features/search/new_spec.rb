fb15863f149095c9fe0d21e94dd502c722d6d259
Will Dunlap <dunlapww@gmail.com>
2020-12-05 14:51:47 -0700

    Add test for discover top 40 and search by keyword
    Co-authored-by: Kate Tester <Katemorris@gmail.com>
    Co-authored-by: George Soderholm <Georgesoderholm@gmail.com>

--- spec/features/search/new_spec.rb
+++ spec/features/search/new_spec.rb
@@ -9,10 +9,16 @@ describe 'Discover Page (/discover)' do
         password_confirmation: '1234**USAusa'
       )
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit '/discover'
     end
     it "I can search for the top 40 movies" do
-      visit '/discover'
       click_button 'Discover Top 40'
+      expect(current_path).to eq '/results'
+    end
+    it "I can search for a movie by title" do
+      fill_in :search, with: "Fight Club"
+      save_and_open_page
+      click_on 'Search'
       expect(current_path).to eq '/results'
     end
   end
