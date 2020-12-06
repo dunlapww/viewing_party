101f3146fecab799673d901dfa3099129a0c900d
George Soderholm <georgesoderholm@gmail.com>
2020-12-06 15:41:10 -0700

    Add test to check the checkboxes appear for each friend
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Kate Tester <katemorris@gmail.com>

---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ViewingParty, type: :model do
+  describe 'relationships' do
+    it { should belong_to :movie }
+    it { should belong_to :user }
+    it { should have_many :attendees }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :duration }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :movie_id }
+  end
+end
