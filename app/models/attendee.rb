8c4e77afda2fcd76974be8f7df5a3f168b0b541d
Kate Tester <katemorris@gmail.com>
2020-12-06 16:11:39 -0700

    Adds test for attendee model
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,3 +1,7 @@
 class Attendee < ApplicationRecord
   belongs_to :viewing_party
+  belongs_to :friend, :class_name => "User"
+
+  validates :friend_id, presence: true
+  validates :viewing_party_id, presence: true
 end
