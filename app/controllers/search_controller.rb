977ba4f54bdc4eeefc11783e9976e2581dbd5397
Will Dunlap <dunlapww@gmail.com>
2020-12-08 14:07:16 -0700

    Pass test for different string searches

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,10 +5,9 @@ class SearchController < ApplicationController
   def show
     if params[:search]
       @movies = MovieSearch.new(params[:search])
-      @search = params[:search]
+      @search = "'#{params[:search]}'"
     else
       @movies = TopMovieSet.new
-      @search = nil
     end
   end
 end
