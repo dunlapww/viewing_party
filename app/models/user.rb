e7a8e89b1a05114df35f594ca0b1050f574b3b58
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 10:48:20 -0700

    Add back in comments that might be helpful to fellow developers

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :viewing_parties
 
   def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-at-least-one-capital-at-least-one-lower-case-at-least-one-special-character
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
 
     msg = 'Password needs to be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character.'
