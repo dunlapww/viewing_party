8a738614c5e20498ea6de73127d47a71d79cb17b
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 14:28:35 -0700

    Add test to check the MovieFacade has a method that returns a Hash with three keys

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,15 +1,10 @@
 class MovieFacade
-  def self.movie_data(uuid)
-    json = MovieService.movie_data
+  def self.movie_details(uuid)
     details = {}
     details[:movie_data] = MovieService.movie_data(uuid)
     details[:cast_data] = MovieService.cast_data(uuid)
     details[:review_data] = MovieService.review_data(uuid)
-  end
-
-  def self.review_data
-  end
-
-  def self.cast_data
+    # binding.pry
+    return details
   end
 end
--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe MovieFacade do
+  describe 'class methods' do
+    it '.movie_details' do
+      VCR.use_cassette('movie_detail_550_movie_details') do
+        results = MovieFacade.movie_details(550)
+
+        expect(results).to be_a Hash
+        expect(results.count).to eq(3)
+      end
+    end
+  end
+end
