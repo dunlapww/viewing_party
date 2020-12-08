acb6be9ad0f0b05e1c6f722688cbe7d29dd57a59
George Soderholm <georgesoderholm@gmail.com>
2020-12-07 20:52:33 -0700

    Add an alias for movie_services_path

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   #movies
-  get '/movies/:uuid', to: 'movie_services#show'
+  get '/movies/:uuid', to: 'movie_services#show', as: 'movie_services'
 
   #viewing_parties
   get '/movies/:uuid/viewing-party/new', to: 'viewing_parties#new'
