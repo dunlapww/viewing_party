8103bf1cc2b32ec49204b7928f5c691e79a71209
George Soderholm <georgesoderholm@gmail.com>
2020-12-09 01:18:34 -0700

    Unskip tests

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 RSpec.describe Movie, type: :model do
-  xdescribe 'validations' do
+  describe 'validations' do
     it { should validate_presence_of :uuid }
     it { should validate_presence_of :title }
   end
 
-  xdescribe 'relationships' do
+  describe 'relationships' do
     it { should have_many :viewing_parties }
   end
 end
