d6819cd4351127949f8bfce2550b1816e8dce60a
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 23:34:11 -0700

    Update the methods to create poros for movie_details, cast_details, review_details

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -4,13 +4,16 @@ class MovieFacade
   end
 
   def self.cast_details(uuid)
-    # this is a hash. with [:id] as the 550 and [:cast] is an array of hashes
-    # Cast.new(MovieService.cast_data(uuid)
-    CastMember.new(MovieService.cast_data(uuid))
+    results =  MovieService.cast_data(uuid)
+    results[:cast].map do |movie_detail|
+      CastMember.new(movie_detail)
+    end
   end
 
   def self.review_details(uuid)
-    # this is a hash. with [:id] as the 550 and [:result] is an array of hashes
-    Review.new(MovieService.review_data(uuid))
+    results =  MovieService.review_data(uuid)
+    results[:results].map do |movie_detail|
+      Review.new(movie_detail)
+    end
   end
 end
--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -7,23 +7,26 @@ RSpec.describe MovieFacade do
         uuid = 550
         results = MovieFacade.movie_details(uuid)
         expect(results).to be_a MovieDetail
-      end
-    end
-
-    it '.cast_details' do
-      VCR.use_cassette('movie_detail_550_review_details') do
-        uuid = 550
-        results = MovieFacade.review_details(uuid)
-        expect(results).to be_a Review
+        expect([results].count).to eq(1)
       end
     end
 
     it '.review_details' do
+      VCR.use_cassette('movie_detail_550_review_details') do
+        uuid = 550
+        results = MovieFacade.review_details(uuid)
+        expect(results).to be_a Array
+        expect(results.first).to be_a Review
+      end
+    end
+
+    it '.cast_details' do
       VCR.use_cassette('movie_detail_550_cast_details') do
         uuid = 550
         results = MovieFacade.cast_details(uuid)
 
-        expect(results).to be_a CastMember
+        expect(results).to be_a Array
+        expect(results.first).to be_a CastMember
       end
     end
   end
