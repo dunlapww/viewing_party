456c7cd0a69a1d8a3b92dbbd1845e09464590bdc
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 17:51:54 -0700

    Add notes from the check in to work toward

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,9 +2,15 @@ class MovieFacade
   def self.movie_details(uuid)
     details = {}
     details[:movie_data] = MovieService.movie_data(uuid)
+    # return data is an arrray
     details[:cast_data] = MovieService.cast_data(uuid)
     details[:review_data] = MovieService.review_data(uuid)
-    # binding.pry
     return details
   end
+  
+  Movie.new(MovieService.movie_data(uuid)
+
+  # cast poro
+  # review poro
+  # movie poro
 end
