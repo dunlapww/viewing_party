735aa53db73b420ca9e37873f6ed94c8e7844d42
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 17:54:30 -0700

    Remove the params from the end of the API calls and iterate thru a do loop to get the params

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,26 +1,35 @@
 class MovieService
   def self.movie_data(uuid)
-    response = connection.get("/3/movie/#{uuid}?api_key=#{ENV['MDB_API_KEY']}")
+    response = connection.get("/3/movie/#{uuid}")
     parse_data(response)
   end
 
   def self.review_data(uuid)
-    response = connection.get("/3/movie/#{uuid}/reviews?api_key=#{ENV['MDB_API_KEY']}")
+    response = connection.get("/3/movie/#{uuid}/reviews")
     parse_data(response)
   end
 
   def self.cast_data(uuid)
-    response = connection.get("/3/movie/#{uuid}/credits?api_key=#{ENV['MDB_API_KEY']}")
+    response = connection.get("/3/movie/#{uuid}/credits")
+    parse_data(response)
+  end
+
+  def self.top_rated(uuid)
+    response = connection.get("/3/movie/#{uuid}")
     parse_data(response)
   end
 
   def self.connection
-    Faraday.new('https://api.themoviedb.org')
+    Faraday.new('https://api.themoviedb.org') do |f|
+      f.params['api_key'] = ENV['MDB_API_KEY']
+    end
   end
 
   def self.parse_data(response)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+
   # def self.movie_details(uuid)
   #   response = conn.get("/3/movie/#{uuid}?api_key=#{ENV['MDB_API_KEY']}")
   #   JSON.parse(response.body, symbolize_names: true)
