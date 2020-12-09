14221b8e0280f2ad62e0183fc0453625805e1892
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 23:36:43 -0700

    Remove the helper method, that was not as clear as keeping the method together

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,17 +1,15 @@
 class SearchFacade
   def self.top_rated(page)
-    @json = MovieService.top_rated(page)
-    parse
+    results = MovieService.top_rated(page)
+    results[:results].map do |movie_detail|
+      MovieDetail.new(movie_detail)
+    end
   end
 
   def self.search(title, page)
-    @json = SearchService.search(title, page)
-    parse
-  end
-
-  def self.parse
-    @json[:results].map do |movie_detail|
-        MovieDetail.new(movie_detail)
+    results = SearchService.search(title, page)
+    results.map do |movie_detail|
+      MovieDetail.new(movie_detail)
     end
   end
 end
--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -2,23 +2,26 @@ require 'rails_helper'
 
 RSpec.describe SearchFacade do
   describe 'class methods' do
-    xit '.top_rated' do
+    it '.search' do
       VCR.use_cassette('search_results') do
         movie_title = 'Fight Club'
         page = 1
         results = SearchFacade.search(movie_title, page)
 
+        expect(results).to be_a Array
+        expect(results.count).to be_between(0, 20)
+        expect(results.first).to be_a MovieDetail
       end
     end
 
-    it '.search' do
+    it '.top_rated' do
       VCR.use_cassette('top_rated_movies_results') do
         page = 1
         results = SearchFacade.top_rated(page)
 
         expect(results).to be_a Array
         expect(results.count).to be_between(0, 20)
-
+        expect(results.first).to be_a MovieDetail
       end
     end
   end
