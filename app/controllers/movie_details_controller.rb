6b0eb795cd5310976973fa22cf1f8734c36e48a6
George Soderholm <georgesoderholm@gmail.com>
2020-12-09 00:20:54 -0700

    Add two instance variables (@cast and @reviews) to allow access in the view

--- app/controllers/movie_details_controller.rb
+++ app/controllers/movie_details_controller.rb
@@ -1,6 +1,8 @@
 class MovieDetailsController < ApplicationController
   def show
     @movie_detail = MovieFacade.movie_details(params[:uuid])
+    @cast = MovieFacade.cast_details(params[:uuid])
+    @reviews = MovieFacade.review_details(params[:uuid])
     # binding.pry
   end
 end
