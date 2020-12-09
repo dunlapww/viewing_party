68c2faaf0160545b85ac0990583447e52550169d
George Soderholm <georgesoderholm@gmail.com>
2020-12-09 00:50:54 -0700

    Rename instance variables to be consistant throughout the app

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,9 @@ class SearchController < ApplicationController
 
   def show
     if params[:search]
-      @movies_details = SearchFacade.search(params[:search], 1)
+      @movie_details = SearchFacade.search(params[:search], 1)
     else
-      @movies_details = SearchFacade.top_rated(1)
+      @movie_details = SearchFacade.top_rated(1)
     end
   end
 end
