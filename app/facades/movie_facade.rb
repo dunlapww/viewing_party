45c3be1125eafb921b0d72d140051ac8eab882d1
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 21:17:05 -0700

    Refactor out two more methods for the reviews and the cast

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,16 +1,16 @@
 class MovieFacade
   def self.movie_details(uuid)
-    details = {}
-    details[:movie_data] = MovieService.movie_data(uuid)
-    # return data is an arrray
-    details[:cast_data] = MovieService.cast_data(uuid)
-    details[:review_data] = MovieService.review_data(uuid)
-    return details
+    Movie.new(MovieService.movie_data(uuid))
   end
 
-  # Movie.new(MovieService.movie_data(uuid)
+  def self.cast_details(uuid)
+    # this is a hash. with [:id] as the 550 and [:cast] is an array of hashes
+    # Cast.new(MovieService.cast_data(uuid)
+    CastMember.new(MovieService.cast_data(uuid)
+  end
 
-  # cast poro
-  # review poro
-  # movie poro
+  def self.review_details(uuid)
+    # this is a hash. with [:id] as the 550 and [:result] is an array of hashes
+    Review.new(MovieService.review_data(uuid)
+  end
 end
