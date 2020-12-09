bffd65ebe450d27ab0061d89faf6378ba03c41f8
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 21:34:48 -0700

    Add tests for a review_spec. Add author, content, review_id and url attributes

--- app/poros/review.rb
+++ app/poros/review.rb
@@ -1,3 +1,12 @@
 class Review
-  
+  attr_reader :author,
+              :content,
+              :review_id,
+              :url
+  def initialize(data)
+    @author = data[:author]
+    @content = data[:content]
+    @review_id = data[:id]
+    @url = data[:url]
+  end
 end
---
+++ spec/poros/reviews_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Review do
+  it 'exists' do
+      review ={
+                  "author": "Goddard",
+                  "author_details": {
+                      "name": "",
+                      "username": "Goddard",
+                      "avatar_path": "/https://secure.gravatar.com/avatar/f248ec34f953bc62cafcbdd81fddd6b6.jpg",
+                      "rating": nil
+                  },
+                  "content": "Pretty awesome movie.  It shows what one crazy person can convince other crazy people to do.  Everyone needs something to believe in.  I recommend Jesus Christ, but they want Tyler Durden.",
+                  "created_at": "2018-06-09T17:51:53.359Z",
+                  "id": "5b1c13b9c3a36848f2026384",
+                  "updated_at": "2018-06-11T02:40:47.833Z",
+                  "url": "https://www.themoviedb.org/review/5b1c13b9c3a36848f2026384"
+                }
+      first_review = Review.new(review)
+      expect(first_review).to be_a Review
+      expect(first_review.author).to eq('Goddard')
+      expect(first_review.review_id).to eq('5b1c13b9c3a36848f2026384')
+      expect(first_review.content).to eq('Pretty awesome movie.  It shows what one crazy person can convince other crazy people to do.  Everyone needs something to believe in.  I recommend Jesus Christ, but they want Tyler Durden.')
+      expect(first_review.url).to eq('https://www.themoviedb.org/review/5b1c13b9c3a36848f2026384')
+  end
+end
