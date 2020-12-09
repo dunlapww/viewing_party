7499c38affb664f3d8b742c3314476b7e1ecc9e8
George Soderholm <georgesoderholm@gmail.com>
2020-12-09 15:26:02 -0700

    Update the arguments needed for top_forty
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Kate Tester <katemorris@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,10 @@ class SearchController < ApplicationController
   def show
     render file: 'public/404' unless current_user
     if params[:search]
-      @movies = SearchFacade.search(params[:search], 1)
+      @movies = SearchFacade.search(params[:search])
       @search = "'#{params[:search]}'"
     else
-      @movies = SearchFacade.top_rated(1)
+      @movies = SearchFacade.top_rated
     end
   end
 end
