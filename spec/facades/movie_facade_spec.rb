be53f37f8027ef6f7b48273cf4adf91cecf2c61c
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 17:56:52 -0700

    Redo cassette

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe MovieFacade do
   describe 'class methods' do
     it '.movie_details' do
       VCR.use_cassette('movie_detail_550_movie_details') do
-        results = MovieFacade.movie_details(550)
+        uuid = 550
+        results = MovieFacade.movie_details(uuid)
 
         expect(results).to be_a Hash
         expect(results.count).to eq(3)
+        expect(results.keys).to eq([:movie_data, :cast_data, :review_data])
+        expect(results.values).to eq([MovieService.movie_data(uuid),  MovieService.cast_data(uuid), MovieService.review_data(uuid)])
       end
     end
   end
