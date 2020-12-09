445a6b5e27eadc50a1af92ffc86e72783f353194
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 22:31:00 -0700

    Remove pry

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,16 +1,16 @@
 class MovieFacade
   def self.movie_details(uuid)
-    Movie.new(MovieService.movie_data(uuid))
+    MovieDetail.new(MovieService.movie_data(uuid))
   end
 
   def self.cast_details(uuid)
     # this is a hash. with [:id] as the 550 and [:cast] is an array of hashes
     # Cast.new(MovieService.cast_data(uuid)
-    CastMember.new(MovieService.cast_data(uuid)
+    CastMember.new(MovieService.cast_data(uuid))
   end
 
   def self.review_details(uuid)
     # this is a hash. with [:id] as the 550 and [:result] is an array of hashes
-    Review.new(MovieService.review_data(uuid)
+    Review.new(MovieService.review_data(uuid))
   end
 end
