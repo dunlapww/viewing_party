d7671661e983e90549f01ee86561b6cebb59977c
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 22:29:58 -0700

    Remove whitespace

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -6,11 +6,24 @@ RSpec.describe MovieFacade do
       VCR.use_cassette('movie_detail_550_movie_details') do
         uuid = 550
         results = MovieFacade.movie_details(uuid)
+        expect(results).to be_a MovieDetail
+      end
+    end
 
-        expect(results).to be_a Hash
-        expect(results.count).to eq(3)
-        expect(results.keys).to eq([:movie_data, :cast_data, :review_data])
-        expect(results.values).to eq([MovieService.movie_data(uuid),  MovieService.cast_data(uuid), MovieService.review_data(uuid)])
+    it '.cast_details' do
+      VCR.use_cassette('movie_detail_550_review_details') do
+        uuid = 550
+        results = MovieFacade.review_details(uuid)
+        expect(results).to be_a Review
+      end
+    end
+
+    it '.review_details' do
+      VCR.use_cassette('movie_detail_550_cast_details') do
+        uuid = 550
+        results = MovieFacade.cast_details(uuid)
+
+        expect(results).to be_a CastMember
       end
     end
   end
