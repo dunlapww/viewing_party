0b4eab1ea46bf60fe953ab3a0f44e581de7c96f1
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 23:08:44 -0700

    Change the setup

--- app/poros/cast.rb
+++ app/poros/cast.rb
@@ -1,26 +1,26 @@
 class Cast
-  attr_reader :name, :character, :picture, :gender, :id
+  # attr_reader :name, :character, :picture, :gender, :id
   def initialize(cast_members)
-    binding.pry
-    @name = cast_member[:name]
-    @character = cast_member[:character]
-    @picture = cast_member[:profile_path]
-    @gender = cast_member[:gender] #this is a number and will need to be formatted
-    @id = cast_member[:id]
+    # binding.pry
+    # @name = cast_member[:name]
+    # @character = cast_member[:character]
+    # @picture = cast_member[:profile_path]
+    # @gender = cast_member[:gender] #this is a number and will need to be formatted
+    # @id = cast_member[:id]
   end
 
-  # def name
-  # end
-  #
-  # def character
-  # end
-  #
-  # def picture
-  # end
-  #
-  # def gender
-  # end
-  #
-  # def id
-  # end
+  def names
+  end
+
+  def characters
+  end
+
+  def pictures
+  end
+
+  def gender
+  end
+
+  def id
+  end
 end
