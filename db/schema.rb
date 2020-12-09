d5d1844bb0a845e42a1bf569bf99581937fa205c
George Soderholm <georgesoderholm@gmail.com>
2020-12-09 16:41:22 -0700

    Remove comments from the schema
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Kate Tester <katemorris@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,10 @@ ActiveRecord::Schema.define(version: 2020_12_06_222926) do
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id"
-    # t.bigint 'friend_id'
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
-    # t.index ["friend_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
