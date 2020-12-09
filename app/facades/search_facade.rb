78f482feda7336d77ef9f4e57c7b5f2202b63e42
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 19:26:45 -0700

    Add tests for the top_rated and search methods

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,9 +1,19 @@
 class SearchFacade
-  def self.top_rated
-
+  def self.top_rated(uuid)
+    @json = MovieService.top_rated(uuid)
+    parse
   end
 
-  def self.search()
-    consolidate_results(search_terms)
+  def self.search(title, page)
+    @json = SearchService.search(title, page)
+    parse
+  end
+
+  def self.parse
+    @movie_details = @json.map do |movie_detail|
+      if movie_detail[0] == :id
+        return MovieDetail.new(movie_detail[1])
+      end
+    end
   end
 end
