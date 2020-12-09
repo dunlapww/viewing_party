4ec1d88a5144ea82344c5f9c8910b777dec9a1a6
George Soderholm <georgesoderholm@gmail.com>
2020-12-08 22:29:25 -0700

    Add method to test the top_rated movies

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe MovieService do
           expect(results[:vote_average]).to be_a(Integer).or be_a Float
         end
       end
+    end
     describe '#review_data' do
       it 'can return review data' do
         VCR.use_cassette('movie_detail_550_movie_service_review') do
@@ -67,6 +68,47 @@ RSpec.describe MovieService do
         end
       end
     end
+    describe '#cast_data' do
+      it 'can return cast data' do
+        VCR.use_cassette('movie_detail_550_movie_service_cast') do
+          cast = MovieService.cast_data(550)
+          expect(cast).to be_a Hash
+
+          expect(cast).to have_key :id
+          expect(cast[:id]).to be_a Integer
+
+          expect(cast).to have_key :cast
+          expect(cast[:cast]).to be_a Array
+
+          expect(cast[:cast][0]).to have_key :name
+          expect(cast[:cast][0][:name]).to be_a String
+
+          expect(cast[:cast][0]).to have_key :character
+          expect(cast[:cast][0][:character]).to be_a String
+        end
+      end
+    end
+    describe '#top_rated' do
+      it 'can return top_rated data' do
+        VCR.use_cassette('top_rated_movies') do
+          top_rated = MovieService.top_rated
+          expect(top_rated).to be_a Hash
+          expect(top_rated[:results].count).to eq(20)
+
+          dantes = top_rated[:results].first 
+          expect(dantes).to have_key :original_title
+          expect(dantes[:original_title]).to be_a String
+
+          expect(dantes).to have_key :id
+          expect(dantes[:id]).to be_a Integer
+
+          expect(dantes).to have_key :vote_average
+          expect(dantes[:vote_average]).to be_a(Integer).or be_a Float
+
+          expect(dantes).to have_key :poster_path
+          expect(dantes[:poster_path]).to be_a String
+        end
+      end
+    end
   end
-end
 end
